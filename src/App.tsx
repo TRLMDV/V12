7d1d9fcbbc5048ace775ab9546a1051cb9ded5f7
[dyad] <ruslan.mammadov.v12@gmail.com>
2025-11-07 16:30:49 +0400

    [dyad] Moved DataProvider to main.tsx to fix context error - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { DataProvider } from "./context/DataContext";
+// DataProvider is now imported and used in main.tsx, so no longer needed here
+// import { DataProvider } from "./context/DataContext";
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
@@ -29,7 +30,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <DataProvider>
+        {/* DataProvider removed from here */}
           <Routes>
             <Route path="/" element={<MainLayout><Dashboard /></MainLayout>} />
             <Route path="/products" element={<MainLayout><Products /></MainLayout>} />
@@ -48,7 +49,6 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </DataProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
