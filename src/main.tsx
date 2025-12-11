25aea8ed113f513c7920e99ffa74bc42844fd184
[dyad] <tural511@gmail.com>
2025-12-11 12:21:57 +0400

    [dyad] Fix createRoot import in main - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './globals.css';
 import { I18nProvider } from './context/I18nProvider';
+import './globals.css';
 import { DataProvider } from "@/context/DataContext.tsx"; // Use alias for consistent import
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode> {/* Wrap App with StrictMode for development checks */}
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <I18nProvider>
       <App />
     </I18nProvider>
