0de1d2ec5bda0307faa2637dc543ce06752b32b0
[dyad] <tural511@gmail.com>
2025-12-15 15:21:14 +0400

    [dyad] Search for duplicate useData declarations - wrote 1 file(s)

--- src/forms/ProductMovementForm.tsx
+++ src/forms/ProductMovementForm.tsx
@@ -6,7 +6,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { useData } from '@/context/DataContext';
 import { t } from '@/utils/i18n';
 import { Id, ProductMovement, SellOrder, Warehouse, Product } from '@/types';
 import { format, parseISO } from 'date-fns';
